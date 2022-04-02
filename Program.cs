127ddf0a6f40f89a32c614aab9e94ea23f3d2a04
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-04-02 16:25:55 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MySql.Data;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
