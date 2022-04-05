dcf288f8ba2a8901c7e934f3e760a4ae3658997d
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-04-05 17:00:13 +0800

    完成字符串协议

--- core/ProtocolBase.cs
+++ core/ProtocolBase.cs
@@ -1,6 +1,47 @@
 ﻿namespace ShootingGameServer.core;
 
+/// <summary>
+/// 协议基类
+/// </summary>
 public class ProtocolBase
 {
-    
+    /// <summary>
+    /// 解码器，解码readBuff中从start开始的length长度的字节
+    /// </summary>
+    /// <param name="readBuff">读缓冲区</param>
+    /// <param name="start">解码的开始位置</param>
+    /// <param name="length">解码的字节长度</param>
+    /// <returns></returns>
+    public virtual ProtocolBase Decode(byte[] readBuff, int start, int length)
+    {
+        return new ProtocolBase();
+    }
+
+    /// <summary>
+    /// 编码器
+    /// </summary>
+    /// <returns></returns>
+    public virtual byte[] Encode()
+    {
+        return new byte[] {};
+    }
+
+    /// <summary>
+    /// 协议名称，用于消息分发
+    /// 消息分发会把不同协议名称的协议交给不同的函数来处理
+    /// </summary>
+    /// <returns>协议名称</returns>
+    public virtual string GetName()
+    {
+        return "";
+    }
+
+    /// <summary>
+    /// 描述 用于调试时比较直观地显示协议的内容
+    /// </summary>
+    /// <returns></returns>
+    public virtual string GetDesc()
+    {
+        return "";
+    }
 }
--- core/ProtocolStr.cs
+++ core/ProtocolStr.cs
@@ -1,6 +1,60 @@
 ﻿namespace ShootingGameServer.core;
 
-public class ProtocolStr
+/// <summary>
+/// 字符串协议模型
+/// 形式 名称, 参数1, 参数2, 参数3
+/// 字符串协议模型有天生的漏洞，只要客户端发送带逗号的信息就会引起混乱，所以只是用来演示
+/// </summary>
+public class ProtocolStr : ProtocolBase
 {
+    // 传输的字符串，整个协议都只用字符串表达
+    public string str;
     
+    /// <summary>
+    /// 解码器，解码过程就是将字节流转换为字符串
+    /// </summary>
+    /// <param name="readBuff">读缓冲区</param>
+    /// <param name="start">解码的开始位置</param>
+    /// <param name="length">解码的字节长度</param>
+    /// <returns></returns>
+    public override ProtocolBase Decode(byte[] readBuff, int start, int length)
+    {
+        ProtocolStr protocol = new ProtocolStr();
+        protocol.str = System.Text.Encoding.UTF8.GetString(readBuff, start, length);
+        return (ProtocolBase)protocol;
+    }
+
+    /// <summary>
+    /// 编码器，编码过程就是将字符串转换成字节流
+    /// </summary>
+    /// <returns></returns>
+    public override byte[] Encode()
+    {
+        byte[] b = System.Text.Encoding.UTF8.GetBytes(str);
+        return b;
+    }
+
+    /// <summary>
+    /// 获取协议名称
+    /// </summary>
+    /// <returns></returns>
+    public override string GetName()
+    {
+        if (str.Length == 0)
+        {
+            return "";
+        }
+        // 第一个逗号前的参数就是协议名称
+        return str.Split(',')[0];
+    }
+
+    /// <summary>
+    /// 协议描述
+    /// </summary>
+    /// <returns></returns>
+    public override string GetDesc()
+    {
+        // 用整个字符串代表协议描述
+        return str;
+    }
 }
